c7aaef038ca85f76701179c3b0a84e3f84fcf633
Andrew Fong <id@andrewfong.com>
2019-08-15 11:19:01 -0700

    Functional main

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,44 +1,22 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { AuthCheck } from './auth-check';
 import { ChecklistsList } from './checklists-list';
 import { CreateChecklist } from './create-checklist';
 import { SigninSignoutButton } from './signin-signout-button';
 
-interface Props {}
-
-interface State {
-	count: number;
-}
-
-export class Main extends PureComponent<Props, State> {
-	constructor(props: Props) {
-		super(props);
-		this.state = {
-			count: 0,
-		};
-		this.handleClick = this.handleClick.bind(this);
-	}
-
-	handleClick() {
-		this.setState(({ count }) => ({
-			count: count + 1,
-		}));
-	}
-
-	render() {
-		return (
-			<div>
-				<nav>
-					<SigninSignoutButton />
-				</nav>
-				<main>
-					<AuthCheck>
-						<h1>Hello World</h1>
-						<ChecklistsList />
-						<CreateChecklist />
-					</AuthCheck>
-				</main>
-			</div>
-		);
-	}
+export function Main() {
+	return (
+		<div>
+			<nav>
+				<SigninSignoutButton />
+			</nav>
+			<main>
+				<AuthCheck>
+					<h1>Hello World</h1>
+					<ChecklistsList />
+					<CreateChecklist />
+				</AuthCheck>
+			</main>
+		</div>
+	);
 }
